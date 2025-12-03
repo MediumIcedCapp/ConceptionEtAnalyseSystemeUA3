3294ddb8afd80a56a95e696f9826af67e808ba77
Danick Auger <danick269@gmail.com>
2025-12-03 17:16:54 -0500

    1er commit pour publier le code en ligne
    1er commit du projet pour le rendre disponible en ligne sur GitHub.

---
+++ test/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComparerNombres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Gestionnaire d'événement pour comparer deux nombres
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Variables
+            int premierNombre = 0;
+            int deuxiemeNombre = 0;
+
+            try
+            {
+                //Convertir les deux valeurs du textbox en entiers
+                premierNombre =  int.Parse(PremiereValeur.Text);
+                deuxiemeNombre = int.Parse(DeuxiemeValeur.Text);
+
+                if (premierNombre > deuxiemeNombre)
+                {
+                    MonLabel.Content = deuxiemeNombre;
+                    //MessageBox.Show(deuxiemeNombre.ToString());
+                }
+                else if (premierNombre < deuxiemeNombre)
+                {
+                    MonLabel.Content = premierNombre;
+                    //MessageBox.Show(premierNombre.ToString());
+                }
+                else
+                {
+                    MonLabel.Content = "Les deux nombres sont égaux";
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MonLabel.Content = "Les deux valeurs doivent être entières";
+            }
+
+            
+
+
+
+        }
+    }
+}
